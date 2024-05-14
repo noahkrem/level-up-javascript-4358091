abaf69d63c50c4db5bf413840d20c9b3b1b184e8
Noah Kremler <72223746+noahkrem@users.noreply.github.com>
2024-05-14 16:19:52 +0000

    Module 1 testing

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,42 @@
 // Write your code here
+
+class Book {
+
+  constructor(Title, Author, ISBN, numCopies) {
+    this.Title = Title;
+    this.Authot = Author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+
+  getAvailability() {
+    if (this.numCopies == 0) {
+      return "out of stock";
+    }
+    else if (this.numCopies < 10) {
+      return "low stock";
+    }
+    else {
+      return "in stock";
+    }
+  }
+
+  sell(numSold) {
+    this.numCopies -= numSold;
+  }
+
+  restock(numStocked) {
+    this.numCopies += numStocked;
+  }
+}
+
+
+// Testing
+const HungerGames = new Book("Hunger Games", "Suzanne Collins", 123919, 5);
+console.log(HungerGames.availability);
+
+HungerGames.restock(12);
+console.log(HungerGames.availability);
+
+HungerGames.sell(17);
+console.log(HungerGames.availability);
